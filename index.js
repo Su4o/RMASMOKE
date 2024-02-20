95dbb73ce0a34ffffb7f5f0eec28c1d738d32f7e
IrzK <127699325+Su4o@users.noreply.github.com>
2024-02-20 13:20:56 +0000

    code will be organized and fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import "flowbite";
 
-    var paragraphs = document.querySelectorAll('p');
-    
-    // Loop through each <p> element and add the 'kmu' class
-    paragraphs.forEach(function(paragraph) {
-        paragraph.classList.add('kmu');
-    });
+var paragraphs = document.querySelectorAll("p");
 
+// Loop through each <p> element and add the 'kmu' class
+paragraphs.forEach(function (paragraph) {
+  paragraph.classList.add("kmu");
+});
