7cc94cbcb234738369109eeeecbdd4bb163bb2a1
IrzK <127699325+Su4o@users.noreply.github.com>
2024-02-21 12:38:50 +0000

    small update 3.0

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 import "flowbite";
-
-var paragraphs = document.querySelectorAll("p");
-
-// Loop through each <p> element and add the 'kmu' class
-paragraphs.forEach(function (paragraph) {
-  paragraph.classList.add("kmu");
-});
